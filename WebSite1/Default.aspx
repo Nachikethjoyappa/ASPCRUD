33517af4f5424b820f2346761911eb938372ae41
nachikethsj <nachiketh.s.j@live.com>
2024-06-22 14:25:06 +0530

    WEb

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div style="width:100%;background-color:cornflowerblue;color:white;">
+       <br />
+       <h2>My First App</h2> 
+       
+    </div>
+    <form id="form1" runat="server">
+        <div>
+            <hr />
+            <div style="text-align:center;width:70%;color:gray" ><h3>Person Details<asp:FileUpload ID="FileUpload1" runat="server" OnDataBinding="FileUpload1_DataBinding" />
+                </h3></div>
+            <hr />
+            <br />
+             
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+           
+            
+            <asp:Button ID="Button2" runat="server" Text="Search" OnClick="Button2_Click1" BackColor="#0066CC" Font-Bold="True" ForeColor="White" Width="82px" />
+             <hr />  <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+                  Width="75%" Height="179px" OnRowCommand="GridView1_RowCommand">
+                <AlternatingRowStyle BackColor="#CCCCCC" />
+                <Columns>
+                    <asp:BoundField DataField="FirstName" HeaderText="FirstName" />
+                    <asp:BoundField DataField="LastName" HeaderText="LastName" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" />
+                    <asp:BoundField DataField="Marks" HeaderText="Marks" />
+                    <asp:BoundField DataField="ID" HeaderText="ID" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:Button ID="Button5" runat="server" CommandName="Select" BackColor="#006699"
+                             Font-Bold="True" ForeColor="White" Text="Select" CommandArgument="<%# Container.DataItemIndex %>" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                <HeaderStyle BackColor="#666699" ForeColor="White" />
+            </asp:GridView>
+          
+            <br />
+              <hr />
+            <asp:HiddenField ID="HiddenField1"  runat="server" />
+            
+           <asp:Label ID="Label2" runat="server" Text="FirstName"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+             <asp:Label ID="Label1" runat="server" Text="LastName"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+           <asp:Label ID="Label3" runat="server" Text="Age"></asp:Label>  
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+          <asp:Label ID="Label4" runat="server" Text="Marks"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+           
+            
+            <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" BackColor="#0066CC" Font-Bold="True" ForeColor="White" Width="82px" />
+            <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" BackColor="#0066CC" Font-Bold="True" ForeColor="White" Width="82px" />
+            
+            <asp:Button ID="Button4" runat="server" Text="Update" OnClick="Button4_Click" BackColor="#0066CC" Font-Bold="True" ForeColor="White" Width="82px" />
+            
+            <br />
+            <hr />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string strcon = ConfigurationManager.ConnectionStrings["AppConnection"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+        //create new sqlconnection and connection to database by using connection string from web.config file  
+        SqlConnection con = new SqlConnection(strcon);
+        con.Open();
+        if (!this.IsPostBack)
+        {
+            refreshdata();
+
+        }
+    }
+
+    public void refreshdata()
+    {
+        using (SqlConnection conn = new SqlConnection(strcon))
+        {
+            conn.Open();
+            string sql = "select * from Person2";
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+            }
+
+
+            //stored procedure
+            // the stored procedure
+
+            //using (SqlCommand cmd = new SqlCommand("SelectAllCustomers", conn))
+            //{
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //    DataSet ds = new DataSet();
+            //    sda.Fill(ds);
+            //    GridView1.DataSource = ds;
+            //    GridView1.DataBind();
+            //}
+
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string sql = "INSERT INTO Person2(ID,LastName,FirstName,Age,Marks) VALUES (@ID,@LastName,@FirstName,@Age,@Marks)";
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(strcon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@ID", 1);
+                    cmd.Parameters.AddWithValue("@LastName", TextBox1.Text);
+                    cmd.Parameters.AddWithValue("@FirstName", TextBox2.Text);
+                    cmd.Parameters.AddWithValue("@Age", TextBox3.Text);
+                    cmd.Parameters.AddWithValue("@Marks", TextBox4.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+         refreshdata();
+         clear();
+        }
+        catch (SqlException ex)
+        {
+            string msg = "Insert Error:";
+            msg += ex.Message;
+        }
+    }
+
+    //protected void Button2_Click(object sender, EventArgs e)
+    //{
+    //    TextBox1.Text = GridView1.SelectedRow.Cells[0].Text;
+    //    TextBox2.Text = GridView1.SelectedRow.Cells[1].Text;
+    //    TextBox3.Text = GridView1.SelectedRow.Cells[2].Text;
+    //    TextBox4.Text = GridView1.SelectedRow.Cells[3].Text;
+    //    HiddenField1.Value = GridView1.SelectedRow.Cells[4].Text;
+    //}
+
+   
+    public void  clear()
+    {
+       TextBox1.Text="";
+        TextBox2.Text = "";
+        TextBox3.Text = "";
+        TextBox4.Text = "";
+    }
+    //protected void GridView1_RowDataBound(object sender, GridViewCommandEventArgs e)
+    //{
+
+
+
+    //    if (e.CommandName == "Select")
+    //    {
+    //        //Determine the RowIndex of the Row whose Button was clicked.
+    //        int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+    //        //Reference the GridView Row.
+    //        GridViewRow row = GridView1.Rows[rowIndex];   
+          
+
+    //        TextBox1.Text = row.Cells[0].Text;
+    //        TextBox2.Text= row.Cells[1].Text;
+    //        TextBox3.Text = row.Cells[2].Text;
+    //        TextBox4.Text = row.Cells[3].Text;
+    //        HiddenField1.Value = row.Cells[4].Text;
+
+            
+    //    }
+    //}
+
+
+
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        using (SqlConnection conn = new SqlConnection(strcon))
+        {
+            conn.Open();
+            string sql = "select * from Person2 where FirstName like '%" +TextBox5.Text+ "%'";
+           
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+            }
+        }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        string sql = "delete from  Person2 where ID=@ID";
+        try
+        {
+
+            using (SqlConnection conn = new SqlConnection(strcon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@ID", HiddenField1.Value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            refreshdata();
+            clear();
+        }
+        catch (SqlException ex)
+        {
+            string msg = "Insert Error:";
+            msg += ex.Message;
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+
+        string sql = "update  Person2 set FirstName='"+TextBox2.Text+"', LastName='"+TextBox1.Text+"' where ID=@ID";
+        try
+        {
+
+            using (SqlConnection conn = new SqlConnection(strcon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@ID", HiddenField1.Value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            refreshdata();
+            clear();
+        }
+        catch (SqlException ex)
+        {
+            
+        }
+    }
+
+    protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+
+        if (e.CommandName == "Select")
+        {
+            //Determine the RowIndex of the Row whose Button was clicked.
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+            //Reference the GridView Row.
+            GridViewRow row = GridView1.Rows[rowIndex];
+
+
+            TextBox1.Text = row.Cells[0].Text;
+            TextBox2.Text = row.Cells[1].Text;
+            TextBox3.Text = row.Cells[2].Text;
+            TextBox4.Text = row.Cells[3].Text;
+            HiddenField1.Value = row.Cells[4].Text;
+
+
+        }
+    }
+
+    protected void FileUpload1_DataBinding(object sender, EventArgs e)
+    {
+
+    }
+}
